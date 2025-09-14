0c63e8a4fa997547bab219d86893c7f0b114bd7b
wangzhengji06 <wangzhengji06@gmail.com>
2025-09-14 17:04:33 +0900

    added functoionality to initialize DVC

---
+++ cybulde/utils/utils.py
@@ -0,0 +1,14 @@
+
+
+import socket
+import logging
+import subprocess
+
+
+def get_logger(name: str) -> logging.Logger:
+    return logging.getLogger(f"[{socket.gethostname()}] {name}")
+
+
+def run_shell_command(cmd: str) -> str:
+    return subprocess.run(cmd, text=True, shell=True, check=True, capture_output=True).stdout
+
--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -1,11 +1,16 @@
+from pathlib import Path
+
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
+from cybulde.utils.data_utils import initialized_dvc
 
 
 @get_config(config_path="../configs", config_name="config")
 def entrypoint(config: Config) -> None:
-    print(config)
-
+    logger = get_logger(PATH(__file__).name)
+    logger.info
+    
+    initialized_dvc()
 
 if __name__ == "__main__":
-    entrypoint()  # type: ignore
+    version_data()
