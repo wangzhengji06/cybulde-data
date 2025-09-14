802c9bc9823d8f6cf598c0b416df35fb44535059
wangzhengji06 <wangzhengji06@gmail.com>
2025-09-14 15:08:10 +0200

    Update version of the data from v0 to v1

--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -1,9 +1,6 @@
-from pathlib import Path
-
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
-from cybulde.utils.data_utils import initialize_dvc, initialize_dvc_storage
-from cybulde.utils.utils import get_logger
+from cybulde.utils.data_utils import commit_to_dvc, initialize_dvc, initialize_dvc_storage
 
 
 @get_config(config_path="../configs", config_name="config")
@@ -11,7 +8,8 @@ def version_data(config: Config) -> None:
     initialize_dvc()
 
     initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
-    
+   
+    commit_to_dvc(config.dvc_raw_data_folder, config.dvc_remote_name)
 
 if __name__ == "__main__":
     version_data()
