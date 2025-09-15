179ca063f4497c05a51fa75ee02dc8bc05b6c5b9
wangzhengji06 <wangzhengji06@gmail.com>
2025-09-15 11:44:05 +0200

    Update version of the data from v1 to v2

--- cybulde/utils/data_utils.py
+++ cybulde/utils/data_utils.py
@@ -36,7 +36,7 @@ def initialize_dvc_storage(dvc_remote_name: str, dvc_remote_url: str) -> None:
 
 
 def commit_to_dvc(dvc_raw_data_folder: str, dvc_remote_name: str) -> None:
-    current_version = ""
+    current_version = run_shell_command("git tag --list | sort -t v -k 2 -g | tail -1 | sed 's/v//'").strip()
     if not current_version:
         current_version = "0"
     next_version = f"v{int(current_version)+1}"
@@ -53,7 +53,7 @@ def commit_to_dvc(dvc_raw_data_folder: str, dvc_remote_name: str) -> None:
 
 def make_new_data_version(dvc_raw_data_folder: str, dvc_remote_name: str) -> None:
     try: 
-        status = run_shell_command("dvc status {dvc_raw_data_folder}.dvc")
+        status = run_shell_command(f"dvc status {dvc_raw_data_folder}.dvc")
         if status == "Data and pipelines are up to date.\n":
             DATA_UTILS_LOGGER.info("Data and pipelines are up to date.")
             return 
--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -1,6 +1,6 @@
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
-from cybulde.utils.data_utils import commit_to_dvc, initialize_dvc, initialize_dvc_storage
+from cybulde.utils.data_utils import initialize_dvc, initialize_dvc_storage, make_new_data_version
 
 
 @get_config(config_path="../configs", config_name="config")
@@ -9,7 +9,7 @@ def version_data(config: Config) -> None:
 
     initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
    
-    commit_to_dvc(config.dvc_raw_data_folder, config.dvc_remote_name)
+    make_new_data_version(config.dvc_raw_data_folder, config.dvc_remote_name)
 
 if __name__ == "__main__":
     version_data()
